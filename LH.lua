2545c1d1d3bf4ed0826cc1942547b32a23ea21c3
Kulakai <79054051+kulakai@users.noreply.github.com>
2021-09-29 11:49:32 -0700

    Update LH.lua

--- LH.lua
+++ LH.lua
@@ -1,1 +1,7 @@
 -- LH.lua
+local f = CreateFrame("Frame");
+local addon = ...; -- The name of the addon folder
+local version = GetAddOnMetadata(addon, "Version");
+local playersChecked = {};
+local initCheck = false;
+local recievedOutOfDateMessage = false;			
